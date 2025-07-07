676b65319078583422f1fe659bf6ce4d51d11679
nkokua <zy013@naver.com>
2025-07-07 13:38:00 +0900

    fix 사용자아이디 조건 수정

--- src/main/java/com/easystudy/arona/user/dto/SignUpRequestDto.java
+++ src/main/java/com/easystudy/arona/user/dto/SignUpRequestDto.java
@@ -12,7 +12,7 @@ import lombok.Getter;
 public class SignUpRequestDto {
 
     @NotBlank(message = "사용자 아이디는 필수 입력 항목입니다.")
-    @Size(min = 4, max = 20, message = "사용자 아이디는 4자 이상 20자 이하로 입력해주세요.")
+    @Size(min = 1, max = 20, message = "사용자 아이디는 4자 이상 20자 이하로 입력해주세요.")
     private String nickname;
 
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
